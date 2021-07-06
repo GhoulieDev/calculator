8acbc9a07cae177c90b43701f9588bb9a497f542
Mark <markrodgers1@hotmail.com>
2021-07-06 17:24:49 +0100

    Minor tweaks to html and css, started JS

--- script.js
+++ script.js
@@ -30,8 +30,48 @@ function operate(operator, num1, num2) {
     }
 }
 
-console.log(operate('+', 5, 10));
-console.log(operate('-', 5, 10));
-console.log(operate('*', 5, 10));
-console.log(operate('/', 5, 10));
+let firstNum = '';
+let digit;
+let operator = '';
+let secondNum = ''
 
+const displayBox = document.getElementById('display');
+const displayNumberTop = document.getElementById('top');
+const displayNumberBottom = document.getElementById('bottom');
+const displayNum = document.createElement('p');
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', storeInput);
+})
+
+function storeInput(event) {
+   
+}
+
+
+
+// if(event.target.classList.contains('operator')) {
+//     firstNum = Number(firstNum);
+//     operator = event.target.textContent;
+//     displayBox.removeChild(displayNums);
+    
+// }else if(event.target.classList.contains('equals')){
+//     secondNum = Number(secondNum);
+//     displayBox.removeChild(displayNums);
+//     result = operate(operator, firstNum, secondNum);
+//     displayNums.textContent = result;
+//     displayBox.appendChild(displayNums);
+    
+    
+// }else if (typeof firstNum == 'string') {
+//     digit = event.target.textContent;
+//     firstNum = firstNum + digit;
+//     displayNums.textContent = firstNum;
+//     displayBox.appendChild(displayNums);
+// }else{
+//     digit = event.target.textContent;
+//     secondNum = secondNum + digit;
+//     displayNums.textContent = secondNum;
+//     displayBox.appendChild(displayNums);
+// }
