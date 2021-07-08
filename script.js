39efaa7a987d03ffb6d546d027cb40807e03a2f5
Mark <markrodgers1@hotmail.com>
2021-07-08 17:36:19 +0100

    Add clear and delete, found(but not fixed) bugs

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+let digits = '';
+let firstNum = '';
+let operator = '';
+let secondNum = '';
+let result;
+let hasDecimal = false;
+
+const displayBoxTop = document.getElementById('top');
+const displayNumTop = document.createElement('p');
+const displayBoxBottom = document.getElementById('bottom');
+const displayNumBottom = document.createElement('p');
+
+const digitBtns = document.querySelectorAll('.digit');
+digitBtns.forEach(digitBtn => {
+    digitBtn.addEventListener('click', storeInput);
+});
+
+const operatorBtns = document.querySelectorAll('.operator');
+operatorBtns.forEach(operatorBtn => {
+    operatorBtn.addEventListener('click', storeOperator)
+});
+
+const equalsBtn = document.querySelector('.equals');
+equalsBtn.addEventListener('click', performOperation);
+
+const clearBtn = document.querySelector('#clear');
+clearBtn.addEventListener('click', reset);
+
+const deleteBtn = document.querySelector('#delete');
+deleteBtn.addEventListener('click', deleteInput);
+
 function add (num1, num2) {
     return num1 + num2;
 }
@@ -30,35 +61,12 @@ function operate(operator, num1, num2) {
     }
 }
 
-let digits = '';
-let firstNum = '';
-let operator = '';
-let secondNum = '';
-let result;
-let hasDecimal = false;
-
-
-const displayBoxTop = document.getElementById('top');
-const displayNumTop = document.createElement('p');
-const displayBoxBottom = document.getElementById('bottom');
-const displayNumBottom = document.createElement('p');
-
-const digitBtns = document.querySelectorAll('.digit');
-digitBtns.forEach(digitBtn => {
-    digitBtn.addEventListener('click', storeInput);
-});
-
-const operatorBtns = document.querySelectorAll('.operator');
-operatorBtns.forEach(operatorBtn => {
-    operatorBtn.addEventListener('click', storeOperator)
-});
-
-const equalsBtn = document.querySelector('.equals');
-equalsBtn.addEventListener('click', performOperation);
-
 function storeInput(event) {
+    
     let input = event.target.textContent;
-    if(digits == '' && input == '.') {
+    if(digits == '' && input == '0'){
+        return;
+    }else if(digits == '' && input == '.') {
         digits = '0.'
         hasDecimal = true;
     }else if(hasDecimal && input == '.'){
@@ -70,6 +78,7 @@ function storeInput(event) {
         }
     }
     updateDisplay(digits);
+   
 }
 
 function storeOperator(event) {
@@ -79,10 +88,12 @@ function storeOperator(event) {
     }else if (operator == '÷') {
         operator = '/';
     }
-
+    
     if(typeof firstNum == 'string') {
         firstNum = Number(digits);
     }
+    
+
     digits = '';
     clearDisplay();
 }
@@ -94,28 +105,58 @@ function updateDisplay(num){
 
 function clearDisplay(){
     displayBoxBottom.textContent = '';
+    
 }
 
 function performOperation(){
-    if(firstNum) {
+    if(firstNum && digits) {
         secondNum = Number(digits);
         clearDisplay();
         console.log('Before calc: ' + firstNum);
         console.log('Before calc: ' + operator);
         console.log('Before calc: ' + secondNum);
         result = operate(operator, firstNum, secondNum);
+        if (String(result).length > 15){
+            result = result.toPrecision(12);
+        }
         updateDisplay(result);
         firstNum = result;
+       
         console.log(result);
     }
-    
 }
+
+function reset() {
+    digits = '';
+    firstNum = '';
+    operator = '';
+    secondNum = '';
+    hasDecimal = false;
+    result = 0;
+    clearDisplay();
+}   
+
+function deleteInput() {
+    if(digits){
+        digits = digits.slice(0,digits.length-1)
+        updateDisplay(digits);
+    }
+}
+
+//REDO AND TIDY UP CODE, PLANNING AHEAD FOR INPUTS AND OUTPUTS ETC
 
 //test floating point arithmatic numbers when timesing
 //eg 1.01 x 3 = 3.030000000002
+//round it to however many digits after the . ?
+
+//error message when divide by 0
+
+//top display
+
+//keybaord support
 
 
-
+//turn if else ifs into switch?
 
 
 
